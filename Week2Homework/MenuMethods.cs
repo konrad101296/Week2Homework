ce4fd35bd232ef9fafb46270ddd77f2d1e35949b
konrad101296 <161070317+konrad101296@users.noreply.github.com>
2024-07-02 17:35:17 +0200

    Add files via upload

---
+++ Week2Homework/MenuMethods.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2Homework
+{
+    public class MenuMethods
+    {
+        List<MenuProperties> menuList;
+        public MenuMethods()
+        {
+            menuList = new List<MenuProperties>();
+        }
+        public void AddNewMenuOption(int id, string name)
+        {
+            MenuProperties menuOption = new MenuProperties() { Id = id, Name = name };
+            menuList.Add(menuOption);
+        }
+        public List<MenuProperties> MenuListOptions()
+        {
+            Console.WriteLine("Welcome in your used car dealer");
+            Console.WriteLine("Choose what you wanna do...");
+
+            foreach (var item in menuList)
+            {
+                Console.WriteLine($"{item.Id}{"."}{item.Name}");
+            }
+
+            Console.WriteLine("Press the apropriate number...");
+
+            return menuList;
+        }
+    }
+}
+
+
